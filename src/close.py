54eb8a8e6cb6ade82e6e7088db9ba6727939e515
leeping-ng <leeping_ng@hotmail.com>
2020-01-24 17:50:58 +0800

    added code to close the box

---
+++ src/close.py
@@ -0,0 +1,24 @@
+import RPi.GPIO as GPIO
+import time
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+pwm = GPIO.PWM(servoPIN, 50) # GPIO 17 for PWM with 50Hz
+
+# start running PWN on pin and sets it to 0
+pwm.start(0)
+
+angle = 0
+duty = angle / 27 +2.5
+
+# close the box
+pwm.ChangeDutyCycle(duty)
+time.sleep(1)
+
+
+
+# clean up everything
+pwm.stop()
+GPIO.cleanup()
